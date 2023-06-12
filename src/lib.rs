68aef10454f0dae8682675506503b63b0967396f
couldbefree <zoriaalexandr@gmail.com>
2023-06-12 15:41:17 +0300

    print with line numbers

--- src/lib.rs
+++ src/lib.rs
@@ -15,11 +15,23 @@ pub struct Config {
 pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
         match open(&filename) {
-            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Err(e) => eprintln!("{}: {}", filename, e),
             Ok(file) => {
-                for line_result in file.lines() {
+                let mut last_num = 0;
+                for (line_num, line_result) in file.lines().enumerate() {
                     let line = line_result?;
-                    println!("{}", line);
+                    if config.number_lines {
+                        println!("{:6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
                 }
             }
         }
